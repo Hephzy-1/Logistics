a43f720ff73348052c122d00143b9296f9be1d06
Hephzy-1 <hzdelight01@gmail.com>
2025-02-11 16:24:23 +0100

    fix: nodemailer error

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,4 +1,4 @@
-import nodemailer, { SendMailOptions, SentMessageInfo } from 'nodemailer';
+import nodemailer from 'nodemailer';
 import environment from '../config/env';
 import { ErrorResponse } from './errorResponse';
 
@@ -12,49 +12,46 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-export const sendOTP = async (otp: string, email: string): Promise<string> => {
-  const mailOptions: SendMailOptions = {
+export const sendOTP = async (otp:string, email:string) => {
+  const mailOptions = {
     from: 'Hephzy',
     to: email,
     subject: 'OTP for Verification',
     text: `Your OTP is: ${otp}`
   };
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (error: Error | null, info: SentMessageInfo) => {
-      if (error) {
-        console.error(error);
-        reject(new ErrorResponse('Email not sent', 500));
-      } else {
-        console.log('Email sent: ' + info.response);
-        resolve(info.response);
-      }
-    });
+  transporter.sendMail(mailOptions, (error: Error | null, info: any) => {
+    if (error) {
+      console.error(error);
+      throw new ErrorResponse('Email not sent', 500);
+    } else {
+      console.log('Email sent' + info.response);
+      return info.response;
+    }
   });
 };
 
-export const sendResetLink = async (token: string, email: string, role: string): Promise<string> => {
-  const mailOptions: SendMailOptions = {
+export const sendResetLink = async (token: string, email: string, role:string) => {
+  const mailOptions = {
     from: 'Hephzy',
     to: email,
     subject: 'Reset Token Link',
     html: `
       <h2>
         Your reset token link is: 
+      
         <b>http://localhost:${environment.PORT}/api/v1/${role}/reset/${token}</b>
       </h2>
-    `
+    ` 
   };
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, (error: Error | null, info: SentMessageInfo) => {
-      if (error) {
-        console.error(error);
-        reject(new ErrorResponse('Email not sent', 500));
-      } else {
-        console.log('Email sent: ' + info.response);
-        resolve(info.response);
-      }
-    });
+  transporter.sendMail(mailOptions, (error: Error | null, info: any) => {
+    if (error) {
+      console.error(error);
+      throw new ErrorResponse('Email not sent', 500);
+    } else {
+      console.log('Email sent' + info.response);
+      return info.response;
+    }
   });
-};
+}
